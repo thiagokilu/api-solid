cbf9f9a916b673af731f42265b04e5cc2de34fea
Thiago Alexandre <thiagokilu34@gmail.com>
2026-08-17 19:11:29 -0300

    add initial structure

--- src/meu.spec.ts
+++ src/meu.spec.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from "vitest";
 
 describe("App", () => {
-  it("should be true", () => {
-    expect(true).toBe(true);
-  });
+	it("should be true", () => {
+		expect(true).toBe(true);
+	});
 });
